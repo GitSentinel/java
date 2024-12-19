d7f459874bd3970b8cc5d597a55aea0411f83e0d
GitSentinel <mahanta.jacinth2021@gmail.com>
2024-12-19 20:20:20 +0530

    Odd Even Check Program

---
+++ EvenOddNum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class EvenOddNum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        if(n%2 == 0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+
+    }
+}
